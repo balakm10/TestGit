63cfbb8512b8a27d952560a576e9e4df53f329be
Bala Krishnan <balakm10@gmail.com>
2016-02-06 00:35:45 +0530

    Initial commit

---
+++ TestGit/src/com/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class HelloWorld {
+
+	public static void display() {
+		System.out.println("Hello World!");
+	}
+	
+	public static void main(String[] args) {
+		
+		display();
+
+	}
+
+}
