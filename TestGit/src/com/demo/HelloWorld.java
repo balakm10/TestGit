70c716a4cc5b56ef7349e3c2f5bd9f77755ecf0f
Balakrishnan Murugesan <balakrishnan.mu@gmail.com>
2016-02-06 00:59:12 +0530

    add welcome msg

--- TestGit/src/com/demo/HelloWorld.java
+++ TestGit/src/com/demo/HelloWorld.java
@@ -3,7 +3,7 @@ package com.demo;
 public class HelloWorld {
 
 	public static void display() {
-		System.out.println("Hello World!");
+		System.out.println("Hello World!"+ " Welcome to Git!");
 	}
 	
 	public static void main(String[] args) {
