611519f4542c9869e88d7059e11c2240911d0a93
Bala Krishnan <balakm10@gmail.com>
2016-05-31 12:47:26 +0530

    Add lastname

--- TestGit/src/com/demo/HelloWorld.java
+++ TestGit/src/com/demo/HelloWorld.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class HelloWorld {
 	
-	private static String name = "Balakrishnan";
+	private static String name = "Balakrishnan Murugesan";
 
 	private static void display() {
 		System.out.println("Hello " + name + "! Welcome to Git!");
